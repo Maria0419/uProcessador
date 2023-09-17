f3dbce8b3abb66521cb2a2a0b8835c27221ffc62
Maria0419 <maria.pazzini1@gmail.com>
2023-09-17 15:33:24 -0300

    ula_arquivo

---
+++ ula.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ula is
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        sig
+    );
+end ula;
+
+architecture rtl of a_ula is
+
+begin
+
+end architecture;
