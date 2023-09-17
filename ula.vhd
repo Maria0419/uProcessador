47ad66d24e0bce4aa87d4bb5dbc84b1ff342f842
Maria0419 <maria.pazzini1@gmail.com>
2023-09-17 16:07:58 -0300

    ula-vhdl

--- ula.vhd
+++ ula.vhd
@@ -4,14 +4,19 @@ use ieee.numeric_std.all;
 
 entity ula is
     port (
-        clk : in std_logic;
-        rst : in std_logic;
-        sig
+        x, y : in unsigned(15 downto 0);
+        op : in unsigned(1 downto 0);
+        saida : out unsigned(15 downto 0)
     );
 end ula;
 
 architecture rtl of a_ula is
-
 begin
+    saida <= x + y when op = "00" else
+             x - y when op = "01" else
+             "00000001" when op = "10" and x>=y else
+             x * y when op = "11" else
+                "00000000";
+
 
 end architecture;
